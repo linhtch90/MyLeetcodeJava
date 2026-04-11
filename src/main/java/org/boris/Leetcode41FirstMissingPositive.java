bf5724aceb043dbb4c7f25f7c47f63791f4e4bb5
Boris Truong <linhtch90@gmail.com>
2026-04-11 16:45:00 +0700

    Leetcide-41 - First missing positive

---
+++ src/main/java/org/boris/Leetcode41FirstMissingPositive.java
@@ -0,0 +1,30 @@
+package org.boris;
+
+public class Leetcode41FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        cyclicSort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    private void cyclicSort(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+}
