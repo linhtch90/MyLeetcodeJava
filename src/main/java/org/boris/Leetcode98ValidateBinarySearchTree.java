6f04399109ba75a1c043c884a2af2884995999a8
Boris Truong <linhtch90@gmail.com>
2026-05-10 22:33:58 +0700

    [Leetcode-98] - Validate binary seaerch tree - Binary search tree - Medium

---
+++ src/main/java/org/boris/Leetcode98ValidateBinarySearchTree.java
@@ -0,0 +1,21 @@
+package org.boris;
+
+import org.boris.models.TreeNode;
+
+public class Leetcode98ValidateBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean validate(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return validate(node.left, min, node.val) && validate(node.right, node.val, max);
+    }
+}
