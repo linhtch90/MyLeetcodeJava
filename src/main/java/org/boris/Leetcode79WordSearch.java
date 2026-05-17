439dce4eabbf724507fde7dd19ea7b0e5e754e79
Boris Truong <linhtch90@gmail.com>
2026-05-17 19:07:24 +0700

    [Leetcode-79] - Word search - Backtracking - Medium

---
+++ src/main/java/org/boris/Leetcode79WordSearch.java
@@ -0,0 +1,52 @@
+package org.boris;
+
+public class Leetcode79WordSearch {
+    private int m;
+    private int n;
+    private String word;
+    private char[][] board;
+
+    public boolean exist(char[][] board, String word) {
+        m = board.length;
+        n = board[0].length;
+        this.word = word;
+        this.board = board;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(int i, int j, int k) {
+        if (k == word.length() - 1) {
+            return board[i][j] == word.charAt(k);
+        }
+
+        if (board[i][j] != word.charAt(k)) {
+            return false;
+        }
+
+        char c = board[i][j];
+        board[i][j] = '0';
+
+        int[] directions = {-1, 0, 1, 0, -1};
+        for (int u = 0; u < 4; u++) {
+            int x = i + directions[u];
+            int y = j + directions[u + 1];
+
+            if (x >= 0 && x < m && y >= 0 && y < n && board[x][y] != '0' && dfs(x, y, k + 1)) {
+                return true;
+            }
+        }
+
+        board[i][j] = c;
+
+        return false;
+    }
+}
